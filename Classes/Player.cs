d96ba51a4830f1a5a9c097333c402d6e4f59806a
Nina Prokopova <nina30600@gmail.com>
2021-04-25 11:44:47 +0500

    moving totoro, collision this bad objects and score in Text

--- Classes/Player.cs
+++ Classes/Player.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using Game.Properties;
 
 namespace Game.Classes
 {
     class Player
     {
+        public Physics physics;
+        public int framesCount = 0; //для синхронизации с основным таймером
+        public int animationCount = 0;
+        public int score = 0;
+        public int life = 5;
+        public bool isInCollision = false;
+        Image image;
+
+        public Player(PointF position, Size size)
+        {
+            physics = new Physics(position, size);
+            framesCount = 0;
+            score = 0;
+            image = new Bitmap("D:\\C# 2\\Totoro-game\\Game\\Images\\totoro.png");
+        }
+
+        public void DrawImage(Graphics g)
+        {
+            /*if (physics.isCrouching)
+            {
+                g.DrawImage(image,
+                    new Rectangle(new Point((int) physics.transform.position.X, (int) physics.transform.position.Y),
+                        new Size(physics.transform.size.Width, physics.transform.size.Height)));
+            }
+            else
+            {
+                g.DrawImage(image,
+                    new Rectangle(new Point((int) physics.transform.position.X, (int) physics.transform.position.Y),
+                        new Size(physics.transform.size.Width, physics.transform.size.Height)));
+            }*/
+
+            g.DrawImage(image,
+                new Rectangle(new Point((int) physics.transform.position.X, (int) physics.transform.position.Y),
+                    new Size(physics.transform.size.Width, physics.transform.size.Height)));
+        }
+
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Windows.Forms;
 using Game.Classes;
+using System.Drawing;
 
 namespace Game
 {
     public sealed partial class Form1 : Form
     {
+        Player player;
         private Timer mainTimer;
+
         public Form1()
         {
             InitializeComponent();
             DoubleBuffered = true;
             Paint += DrawGame;
+            KeyUp += OnKeyBoardUp;
+            KeyDown += OnKeyBoardDown;
             mainTimer = new Timer();
             mainTimer.Interval = 5;
             mainTimer.Tick += Update;
@@ -22,12 +27,29 @@ namespace Game
         private void Init()
         {
             GameController.Init();
+            player = new Player(new PointF(275, 570), new Size(150, 200));
             mainTimer.Start();
             Invalidate();
         }
 
         private void Update(object sender, EventArgs e)
         {
+            var isCollide = player.physics.Collide();
+            if (isCollide && !player.isInCollision)
+            {
+                player.life -= 1;
+                player.isInCollision = true;
+            }
+
+            if (!isCollide)
+            {
+                player.isInCollision = false;
+            }
+
+            Text = "Totoro - life: " + player.life;
+            if (player.life == 0)
+                mainTimer.Stop();
+            player.physics.ApplyPhisics();
             GameController.MoveMap();
             Invalidate();
         }
@@ -36,7 +58,44 @@ namespace Game
         {
             var g = e.Graphics;
             GameController.DrawObjects(g);
+            player.DrawImage(g);
+        }
+
+        private void OnKeyBoardDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Down:
+                    if (!player.physics.isJumping)
+                    {
+                        player.physics.isCrouching = true;
+                        player.physics.isJumping = false;
+                        player.physics.transform.size.Height = 125;
+                        player.physics.transform.position.Y = 650;
+                    }
+
+                    break;
+            }
+        }
+
+        private void OnKeyBoardUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    if (!player.physics.isJumping)
+                    {
+                        player.physics.isCrouching = false;
+                        player.physics.AddForce();
+                    }
+
+                    break;
+                case Keys.Down:
+                    player.physics.isCrouching = false;
+                    player.physics.transform.size.Height = 200;
+                    player.physics.transform.position.Y = 570;
+                    break;
+            }
         }
     }
 }
-
